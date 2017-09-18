3b9b57fe6e4a4e6013453ca2fc1e1eaf058af58c
pptang <donaldduck518@gmail.com>
2017-09-18 11:25:53 +0900

    Add debug function for rxjs and flow type check

--- frontend/state/RxState.js
+++ frontend/state/RxState.js
@@ -1,24 +1,63 @@
-import React, { Component } from 'react';
+// @flow
+
+import * as React from 'react';
 import Rx from 'rxjs';
+import { Observable } from 'rxjs/Observable';
 
 import reducer$ from '../reducers';
 
-export function createState(reducerStream, initialState$ = Rx.Observable.of({})) {
+const debuggerOn = true;
+Observable.prototype.debug = function (message: string): Observable {
+  return this.do(
+    (next) => {
+      if (debuggerOn) {
+        console.log(message, next);
+      }
+    },
+    (err) => {
+      if (debuggerOn) {
+        console.error('ERROR >>> ', message, err);
+      }
+    },
+    () => {
+      if (debuggerOn) {
+        console.log('Completed.');
+      }
+    },
+  );
+};
+
+export function createState(
+  reducerStream: Observable,
+  initialState$: Observable = Observable.of({}),
+): Observable {
   return initialState$
     .merge(reducerStream)
+    .debug(() => console.log('createState'))
     .scan((state, [scope, reducer]) => ({ ...state, [scope]: reducer(state[scope]) }))
     .publishReplay(1)
     .refCount();
 }
 
-export function connect(selector = state => state, actionSubjects) {
+export function connect(
+  selector?: any => any = state => state,
+  actionSubjects: { [key: string]: Rx.Subject },
+): React.ComponentType<any> => React.ComponentType<any> {
   const actions = Object.keys(actionSubjects)
-    .reduce((acc, key) => ({ ...acc, [key]: (value) => {
-      actionSubjects[key].next(value);
-    }}), {});
+    .reduce((acc, key) => ({
+      ...acc,
+      [key]: (value) => {
+        actionSubjects[key].next(value);
+      },
+    }), {});
 
-  return function wrapWithConnect(WrappedComponent) {
-    return class Connect extends Component {
+  return function wrapWithConnect(
+    WrappedComponent: React.ComponentType<any>,
+  ): React.ComponentType<any> {
+    return class Connect extends React.Component<{}> {
+      // eslint-disable-next-line react/sort-comp
+      subscription: Observable;
+
       componentWillMount() {
         this.subscription = createState(reducer$).map(selector).subscribe(this.setState.bind(this));
       }
