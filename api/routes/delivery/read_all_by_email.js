b6386412978403723a3af58f2a1fc4e182cf1f8f
nkfly <nkflyfly@gmail.com>
2017-10-18 01:09:04 +0900

    fix delivery list & delivery item format

--- api/routes/delivery/read_all_by_email.js
+++ api/routes/delivery/read_all_by_email.js
@@ -20,7 +20,19 @@ module.exports = (req, res) => {
         res.status(500).json({ status: constants.fail });
         return;
       }
-      res.json({ status: constants.success, deliveryList });
+
+      const processedDeliveryList = {}
+      processedDeliveryList.meetupList = [];
+      processedDeliveryList.shippingList = [];
+      deliveryList.forEach(function(delivery) {
+        if (delivery.type == constants.delivery_type.meetup) {
+          processedDeliveryList.meetupList.push(delivery.toMeetup());
+        } else if (delivery.type == constants.delivery_type.shipping) {
+          processedDeliveryList.shippingList.push(delivery.toShipping());
+        }
+      });
+
+      res.json({ status: constants.success, deliveryList: processedDeliveryList });
     });
   });
 };
