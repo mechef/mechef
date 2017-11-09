a4e01cf3fe003bb070c9499fbb8448839550f24e
nkfly <nkflyfly@gmail.com>
2017-11-09 22:18:53 +0900

    change to provide jwt alternative to get menu list

--- api/routes/menu/read_all_by_id.js
+++ api/routes/menu/read_all_by_id.js
@@ -1,20 +1,40 @@
 const Menu = require('../../models/menu');
 const Seller = require('../../models/seller');
 const constants = require('../../utils/constants');
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res) => {
-  Seller.findById(req.query.id, (err, seller) => {
-    if (err) {
-      res.status(404).json({ status: constants.fail, reason: constants.id_not_found });
-      return;
-    }
-    const query = Menu.find({ email: seller.email });
-    query.then((menu) => {
-      if (menu) {
-        res.json({ status: constants.success, menuList: menu });
-      } else {
-        res.status(404).json({ status: constants.fail, reason: constants.email_not_found });
+  const token = req.headers.authorization;
+  if (!token) {
+    Seller.findById(req.query.id, (err, seller) => {
+      if (err) {
+        res.status(404).json({ status: constants.fail, reason: constants.id_not_found });
+        return;
       }
+      const query = Menu.find({ email: seller.email });
+      query.then((menu) => {
+        if (menu) {
+          res.json({ status: constants.success, menuList: menu });
+        } else {
+          res.status(404).json({ status: constants.fail, reason: constants.email_not_found });
+        }
+      });
     });
-  });
+  } else {
+    jwt.verify(token, constants.secret, (err, decoded) => {
+      if (err) {
+        res.status(404).json({ status: constants.fail });
+        return;
+      }
+
+      const query = Menu.find({ email: decoded.email });
+      query.then((menu) => {
+        if (menu) {
+          res.json({ status: constants.success, menuList: menu });
+        } else {
+          res.status(404).json({ status: constants.fail, reason: constants.email_not_found });
+        }
+      });
+    });
+  }
 };
