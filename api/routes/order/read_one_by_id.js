ce0ba78f71d21115cb4f133468fb9d79e090fe0b
Hung, Liyuan a | Yuan | TRVDD <liyuan.a.hung@rakuten.com>
2017-11-24 16:51:40 +0900

    modify order

--- api/routes/order/read_one_by_id.js
+++ api/routes/order/read_one_by_id.js
@@ -1,12 +1,28 @@
 const Order = require('../../models/order');
 const constants = require('../../utils/constants');
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res) => {
-  Order.findById(req.params.id, (err, order) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    res.status(400).json({ status: constants.fail, reason: constants.no_token });
+    return;
+  }
+
+  jwt.verify(token, constants.secret, (err, decoded) => {
     if (err) {
-      res.status(500).json({ status: constants.fail, reason: constants.id_not_found });
+      res.status(500).json({ status: constants.fail });
       return;
     }
-    res.json({ status: constants.success, order });
+    const condition = { sellerEmail: decoded.email, _id: req.params.id };
+
+    const query = Order.findOne(condition);
+    query.then((order) => {
+      if (order) {
+        res.json({ status: constants.success, order });
+      } else {
+        res.status(400).json({ status: constants.fail, reason: constants.email_not_found });
+      }
+    });
   });
 };
