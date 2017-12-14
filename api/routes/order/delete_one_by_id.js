582a38cd280b74c92066ddc9860ec4fb7227e9d7
nkfly <nkflyfly@gmail.com>
2017-12-14 18:15:24 +0900

    add delete order api

---
+++ api/routes/order/delete_one_by_id.js
@@ -0,0 +1,26 @@
+const Order = require('../../models/order');
+const constants = require('../../utils/constants');
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    res.status(400).json({ status: constants.fail, reason: constants.no_token });
+    return;
+  }
+
+  jwt.verify(token, constants.secret, (err, decoded) => {
+    if (err) {
+      res.status(500).json({ status: constants.fail });
+      return;
+    }
+
+    Order.remove({ _id: req.params.id, email: decoded.email }, (e) => {
+      if (e) {
+        res.status(500).json({ status: constants.fail });
+        return;
+      }
+      res.json({ status: constants.success });
+    });
+  });
+};
--- api/routes/order/index.js
+++ api/routes/order/index.js
@@ -3,9 +3,11 @@ const create = require('./create');
 const readAllByState = require('./read_all_by_state');
 const readOneById = require('./read_one_by_id');
 const updateOneById = require('./update_one_by_id');
+const deleteOneById = require('./delete_one_by_id');
 
 order.post('/', create);
 order.get('/', readAllByState);
 order.get('/:id', readOneById);
 order.patch('/:id', updateOneById);
+order.delete('/:id', deleteOneById);
 module.exports = order;
