7f11c7a1a21a89bff0ac7cea49856e98c4158bad
saranron <saranron@gmail.com>
2018-01-01 17:41:38 +0900

    add link to header

--- frontend/components/BuyerHeader.js
+++ frontend/components/BuyerHeader.js
@@ -5,9 +5,15 @@ import CartButton from './CartButton';
 
 const BuyerHeader = () => (
   <div className="buyer-header">
-    <img className="logo" src="/static/food.png" alt="mechef" />
-    <div className="buyerContainer">
-      <CartButton itemCount={100} onCartClicked={() => console.log('cart is clicked')} />
+    <div className="buyer-header--left">
+      <img className="buyer-header__logo" src="/static/food.png" alt="mechef" />
+    </div>
+    <div className="buyer-header--right">
+      <span className="buyer-header__link">FAQ</span>
+      <span className="buyer-header__link">HOW IT WORKS</span>
+      <span className="buyer-header__cart">
+        <CartButton itemCount={100} onCartClicked={() => console.log('cart is clicked')} />
+      </span>
     </div>
     <style jsx>
       {`
@@ -19,21 +25,30 @@ const BuyerHeader = () => (
           height: 90px;
           background-color: #ffffff;
           box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.16);
+          font-family: Ubuntu;
         }
-        .logo {
-          height: 70px;
-          width: 113px;
+        .buyer-header--left {
           margin: 13px 0px 7px 89px;
         }
-        .buyerContainer {
+        .buyer-header__logo {
+          height: 70px;
+          width: 113px;
+        }
+        .buyer-header--right {
           padding-right: 100px;
           height: 30px;
+          display: inline-flex;
+          flex-grow: 1;
+          align-items: center;
+          justify-content: flex-end;
         }
-        .buyerAvatar {
-          width: 30px;
-          height: 30px;
-          border-radius: 15px;
+        .buyer-header__link {
           cursor: pointer;
+          font-size: 15px;
+          line-height: 1;
+          letter-spacing: 0.6;
+          color: #4a4a4a;
+          margin-right: 30px;
         }
       `}
     </style>
