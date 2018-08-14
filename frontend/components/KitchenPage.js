57f7814e8432fe2dc0aefc326254a9dd71a40a8b
pptang <donaldduck518@gmail.com>
2018-08-15 04:11:51 +0900

    Refactor KitchenPage into SFC with withState and withHandler

--- frontend/components/KitchenPage.js
+++ frontend/components/KitchenPage.js
@@ -3,7 +3,7 @@
 import * as React from 'react';
 import Router from 'next/router';
 import Rx from 'rxjs/Rx';
-import { branch } from 'recompose';
+import { branch, compose, withHandlers, withState } from 'recompose';
 
 import { connect } from '../state/RxState';
 import cartActions from '../actions/cartActions';
@@ -23,134 +23,76 @@ type Props = {
   kitchen: KitchenObject,
   kitchenName: string,
   addToCart$: (order: DishOrderType) => Rx.Observable,
+  onDishSelected: (dishId: string) => any,
+  showDishModal: (dishId: string) => any,
+  selectedDish: MenuObject,
+  closeDishModal: () => any,
+  addDishToCart: (dishOrder: DishOrderType) => Rx.Observable,
 };
 
-type State = {
-  displayedProduct?: MenuObject,
-};
-
-class KitchenPage extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-
-    this.state = {
-      displayedProduct: undefined,
-    };
-
-    this.showDishModal = this.showDishModal.bind(this);
-    this.closeDishModal = this.closeDishModal.bind(this);
-    this.onDishSelected = this.onDishSelected.bind(this);
-    this.addDishToCart = this.addDishToCart.bind(this);
-  }
-
-  onDishSelected: Function;
-
-  onDishSelected(dishId: string) {
-    Router.push(
-      {
-        pathname: '/kitchen',
-        query: {
-          kitchen: this.props.kitchenName,
-          dish: dishId,
-        },
-      },
-      `/kitchen/${this.props.kitchenName}/${dishId}`,
-    );
-  }
-
-  closeDishModal: Function;
-
-  closeDishModal() {
-    this.setState({ displayedProduct: undefined });
-  }
-
-  showDishModal: Function;
-
-  showDishModal(dishId: string) {
-    const found =
-      this.props.kitchen.menuList &&
-      this.props.kitchen.menuList.find(dish => dish._id === dishId);
-    this.setState({ displayedProduct: found });
-  }
-
-  addDishToCart: Function;
-
-  addDishToCart(dishOrder: DishOrderType) {
-    const order = {
-      kitchen: this.props.kitchen.kitchenName,
-      ...dishOrder,
-    };
-    this.props.addToCart$(order);
-  }
-
-  renderDishes: Function;
-
-  renderDishes = () =>
-    this.props.kitchen.menuList &&
-    this.props.kitchen.menuList.map(dish => (
-      <DishCard
-        {...dish}
-        key={dish._id}
-        onDishSelected={this.onDishSelected}
-        onAddToCartClick={() => this.showDishModal(dish._id)}
-      />
-    ));
-
-  render() {
-    const { kitchen } = this.props;
-
-    return (
-      <div className="kitchen-main">
-        <KitchenHeader
-          name={kitchen.kitchenName}
-          description={kitchen.kitchenDescription}
-          profileImage={kitchen.profileImage}
+const KitchenPage = ({
+  kitchen,
+  onDishSelected,
+  showDishModal,
+  selectedDish,
+  closeDishModal,
+  addDishToCart,
+}: Props) => (
+  <div className="kitchen-main">
+    <KitchenHeader
+      name={kitchen.kitchenName}
+      description={kitchen.kitchenDescription}
+      profileImage={kitchen.profileImage}
+    />
+    <div className="kitchen-display">
+      {kitchen.menuList && kitchen.menuList.length > 0 ? (
+        kitchen.menuList &&
+        kitchen.menuList.map(dish => (
+          <DishCard
+            {...dish}
+            key={dish._id}
+            onDishSelected={onDishSelected}
+            onAddToCartClick={() => showDishModal(dish._id)}
+          />
+        ))
+      ) : !kitchen.isLoading ? (
+        <KitchenClosedComponent />
+      ) : null}
+      {selectedDish ? (
+        <DishModal
+          dish={selectedDish}
+          onClose={closeDishModal}
+          onDishAdded={addDishToCart}
         />
-        <div className="kitchen-display">
-          {kitchen.menuList && kitchen.menuList.length > 0 ? (
-            this.renderDishes()
-          ) : !kitchen.isLoading ? (
-            <KitchenClosedComponent />
-          ) : null}
-          {this.state.displayedProduct ? (
-            <DishModal
-              dish={this.state.displayedProduct}
-              onClose={this.closeDishModal}
-              onDishAdded={this.addDishToCart}
-            />
-          ) : (
-            ''
-          )}
-        </div>
-        <style jsx>
-          {`
-            .kitchen-main {
-              padding: 0 16px;
-            }
-            .kitchen-display {
-              margin: 18px auto 40px;
-              width: 100%;
-              display: flex;
-              flex-direction: column;
-              align-items: center;
-            }
-            @media (min-width: ${smallBreak}) {
-              .kitchen-main {
-                padding-left: 110px;
-              }
-              .kitchen-display {
-                margin: 70px auto 144px;
-                flex-direction: row;
-                align-items: flex-start;
-                flex-wrap: wrap;
-              }
-            }
-          `}
-        </style>
-      </div>
-    );
-  }
-}
+      ) : null}
+    </div>
+    <style jsx>
+      {`
+        .kitchen-main {
+          padding: 0 16px;
+        }
+        .kitchen-display {
+          margin: 18px auto 40px;
+          width: 100%;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
+        @media (min-width: ${smallBreak}) {
+          .kitchen-main {
+            padding-left: 110px;
+          }
+          .kitchen-display {
+            margin: 70px auto 144px;
+            flex-direction: row;
+            align-items: flex-start;
+            flex-wrap: wrap;
+          }
+        }
+      `}
+    </style>
+  </div>
+);
 
 const KitchenNotFound = () => () => (
   <div className="kitchen-not-found">
@@ -171,8 +113,40 @@ const KitchenNotFound = () => () => (
 
 const showNotFoundPageIfNotKitchen = predicate =>
   branch(predicate, KitchenNotFound);
-const enhance = showNotFoundPageIfNotKitchen(
-  ({ kitchen }) => !kitchen.isLoading && !kitchen.kitchenName,
+const enhance = compose(
+  withState('selectedDish', 'updateSelectedDish', null),
+  withHandlers({
+    onDishSelected: ({ kitchen }) => (dishId: string) => {
+      Router.push(
+        {
+          pathname: '/kitchen',
+          query: {
+            kitchen: kitchen.kitchenName,
+            dish: dishId,
+          },
+        },
+        `/kitchen/${kitchen.kitchenName}/${dishId}`,
+      );
+    },
+    addDishToCart: ({ kitchen, addToCart$ }) => (dishOrder: DishOrderType) => {
+      const order = {
+        kitchen: kitchen.kitchenName,
+        ...dishOrder,
+      };
+      addToCart$(order);
+    },
+    showDishModal: ({ kitchen, updateSelectedDish }) => (dishId: string) => {
+      const selectedDish =
+        kitchen.menuList && kitchen.menuList.find(dish => dish._id === dishId);
+      updateSelectedDish(selectedDish);
+    },
+    closeDishModal: ({ updateSelectedDish }) => () => {
+      updateSelectedDish(null);
+    },
+  }),
+  showNotFoundPageIfNotKitchen(
+    ({ kitchen }) => !kitchen.isLoading && !kitchen.kitchenName,
+  ),
 );
 
 const actionSubjects = {
