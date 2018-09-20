e967b469cbedf572947d2bad355c73b91fd97afd
Bible <ptang8@bus.illinois.edu>
2018-09-20 23:17:32 +0900

    Add delete confirm modal (#207)
    * Add delete confirm modal
    
    * Add modal with action buttons
    
    * Update snapshot

--- frontend/components/MenuItem.js
+++ frontend/components/MenuItem.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import Rx from 'rxjs/Rx';
 import Media from 'react-media';
+import { compose, withHandlers, withState } from 'recompose';
 
 import {
   borderRadius,
@@ -17,6 +18,7 @@ import {
 } from '../utils/styleVariables';
 import PublishButton from './PublishButton';
 import EditDeleteGroup from './EditDeleteGroup';
+import ModalWithActionButton from './ModalWithActionButton';
 
 type Props = {
   dishName: string,
@@ -26,6 +28,9 @@ type Props = {
   onTogglePublish: () => Rx.Observable,
   onEdit: () => Rx.Observable,
   onDelete: () => Rx.Observable,
+  isShowingDeleteConfirmModal: boolean,
+  showDeleteConfirmModal: Function,
+  hideDeleteConfirmModal: Function,
 };
 
 const MenuItem = ({
@@ -36,123 +41,156 @@ const MenuItem = ({
   onTogglePublish,
   onEdit,
   onDelete,
+  isShowingDeleteConfirmModal,
+  showDeleteConfirmModal,
+  hideDeleteConfirmModal,
 }: Props) => (
-  <Media query="(max-width: 768px)">
-    {matches =>
-      matches ? (
-        <article className="mobileContainer">
-          <div className="mobileThumbnail" />
-          <div className="content">
-            <h3>{dishName}</h3>
-            <p className="description">{description}</p>
-            <div className="actionGroup">
+  <div>
+    {isShowingDeleteConfirmModal ? (
+      <ModalWithActionButton
+        title="Delete Menu"
+        message="Do you really want to delete this menu?"
+        onAction={onDelete}
+        actionTitle="Delete"
+        onCancel={hideDeleteConfirmModal}
+        cancelTitle="Cancel"
+      />
+    ) : null}
+    <Media query="(max-width: 768px)">
+      {matches =>
+        matches ? (
+          <article className="mobileContainer">
+            <div className="mobileThumbnail" />
+            <div className="content">
+              <h3>{dishName}</h3>
+              <p className="description">{description}</p>
+              <div className="actionGroup">
+                <PublishButton
+                  isPublish={isPublish}
+                  onTogglePublish={onTogglePublish}
+                />
+                <EditDeleteGroup
+                  onEdit={onEdit}
+                  onDelete={showDeleteConfirmModal}
+                />
+              </div>
+            </div>
+            <style jsx>{`
+                  .mobileContainer {
+                    width: 100%;
+                    background-color: ${whiteColor};
+                    border-radius: ${borderRadius};
+                  }
+                  .mobileThumbnail {
+                    background-image: url('${thumbnailUrl}'), url('../static/img/pancake.jpg');
+                    background-size: cover;
+                    background-position: center;
+                    width: 100%;
+                    height: 100px;
+                    border-top-left-radius: ${borderRadius};
+                    border-bottom-left-radius: ${borderRadius};
+                  }
+                  .content {
+                    padding: 20px;
+                  }
+                  .description {
+                    font-size: 12px;
+                  }
+                  .actionGroup {
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+                  }
+                `}</style>
+          </article>
+        ) : (
+          <div className="menuItemContainer">
+            <div className="menuThumbnail" />
+            <div className="menuText">
+              <div className="topPart">
+                <div className="titleDiv">
+                  <span className="title">{dishName}</span>
+                  <EditDeleteGroup
+                    onEdit={onEdit}
+                    onDelete={showDeleteConfirmModal}
+                  />
+                </div>
+                <span className="description">{description}</span>
+              </div>
               <PublishButton
                 isPublish={isPublish}
                 onTogglePublish={onTogglePublish}
               />
-              <EditDeleteGroup onEdit={onEdit} onDelete={onDelete} />
             </div>
-          </div>
-          <style jsx>{`
-                .mobileContainer {
+            <style jsx>
+              {`
+                .menuItemContainer {
+                  display: flex;
+                  max-width: 600px;
                   width: 100%;
-                  background-color: ${whiteColor};
+                  height: 161px;
                   border-radius: ${borderRadius};
+                  background-color: ${whiteColor};
                 }
-                .mobileThumbnail {
+
+                .menuThumbnail {
                   background-image: url('${thumbnailUrl}'), url('../static/img/pancake.jpg');
                   background-size: cover;
                   background-position: center;
-                  width: 100%;
-                  height: 100px;
+                  width: 161px;
+                  height: 161px;
                   border-top-left-radius: ${borderRadius};
                   border-bottom-left-radius: ${borderRadius};
                 }
-                .content {
+
+                .menuText {
+                  flex: 1;
+                  display: flex;
+                  flex-direction: column;
+                  justify-content: space-between;
+                  margin-left: 20px;
                   padding: 20px;
                 }
-                .description {
-                  font-size: 12px;
+
+                .topPart {
+                  display: flex;
+                  flex-direction: column;
                 }
-                .actionGroup {
+
+                .titleDiv {
                   display: flex;
                   justify-content: space-between;
-                  align-items: center;
+                  margin-bottom: 12px;
                 }
-              `}</style>
-        </article>
-      ) : (
-        <div className="menuItemContainer">
-          <div className="menuThumbnail" />
-          <div className="menuText">
-            <div className="topPart">
-              <div className="titleDiv">
-                <span className="title">{dishName}</span>
-                <EditDeleteGroup onEdit={onEdit} onDelete={onDelete} />
-              </div>
-              <span className="description">{description}</span>
-            </div>
-            <PublishButton
-              isPublish={isPublish}
-              onTogglePublish={onTogglePublish}
-            />
+
+                .title {
+                  font-size: ${titleFontSize};
+                  color: ${textColor};
+                }
+
+                .description {
+                  font-size: ${subtitleFontSize};
+                  color: ${textColor};
+                }
+              `}
+            </style>
           </div>
-          <style jsx>
-            {`
-              .menuItemContainer {
-                display: flex;
-                max-width: 600px;
-                width: 100%;
-                height: 161px;
-                border-radius: ${borderRadius};
-                background-color: ${whiteColor};
-              }
-
-              .menuThumbnail {
-                background-image: url('${thumbnailUrl}'), url('../static/img/pancake.jpg');
-                background-size: cover;
-                background-position: center;
-                width: 161px;
-                height: 161px;
-                border-top-left-radius: ${borderRadius};
-                border-bottom-left-radius: ${borderRadius};
-              }
-
-              .menuText {
-                flex: 1;
-                display: flex;
-                flex-direction: column;
-                justify-content: space-between;
-                margin-left: 20px;
-                padding: 20px;
-              }
-
-              .topPart {
-                display: flex;
-                flex-direction: column;
-              }
-
-              .titleDiv {
-                display: flex;
-                justify-content: space-between;
-                margin-bottom: 12px;
-              }
-
-              .title {
-                font-size: ${titleFontSize};
-                color: ${textColor};
-              }
-
-              .description {
-                font-size: ${subtitleFontSize};
-                color: ${textColor};
-              }
-            `}
-          </style>
-        </div>
-      )
-    }
-  </Media>
+        )
+      }
+    </Media>
+  </div>
 );
 
-export default MenuItem;
+const enhance = compose(
+  withState('isShowingDeleteConfirmModal', 'toggleDeleteConfirmModal', false),
+  withHandlers({
+    showDeleteConfirmModal: ({ toggleDeleteConfirmModal }) => () => {
+      toggleDeleteConfirmModal(true);
+    },
+    hideDeleteConfirmModal: ({ toggleDeleteConfirmModal }) => () => {
+      toggleDeleteConfirmModal(false);
+    },
+  }),
+);
+
+export default enhance(MenuItem);
