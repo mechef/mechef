08ba68a4fdee722507b51bae2d0178acb656aeef
pptang <donaldduck518@gmail.com>
2019-01-19 12:38:25 +0900

    Add webpack bundle analyzer

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,18 @@
-module.exports = {
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
+
+module.exports = withBundleAnalyzer({
+  analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: 'static',
+      reportFilename: '../bundles/server.html',
+    },
+    browser: {
+      analyzerMode: 'static',
+      reportFilename: '../bundles/client.html',
+    },
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
     // Perform customizations to webpack config
     // Important: return the modified config
@@ -20,4 +34,4 @@ module.exports = {
     // Important: return the modified config
     return config;
   },
-};
+});
